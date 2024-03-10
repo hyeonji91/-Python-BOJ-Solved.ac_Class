2296ae089d816c3b504281b80e3390c481dd8b14
hyeonji91 <112065014+hyeonji91@users.noreply.github.com>
2024-03-11 00:36:00 +0900

    2024.03.16

---
+++ class1/[8958] OX_Quiz.py
@@ -0,0 +1,14 @@
+num = int(input())
+
+for i in range(num):
+    temp = 0 #중간 점수
+    score = 0 #최종 점수
+    test = input()
+    for j in test:
+        if(j == 'O'):
+            temp = temp+1
+            score = score + temp
+        else:
+            temp =0
+        print(j +":"+str(score))
+    print(score)
