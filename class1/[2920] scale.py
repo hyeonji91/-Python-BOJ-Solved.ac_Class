977d22ceb4441e4280e5a90c8135962bb8e5a08f
hyeonji91 <112065014+hyeonji91@users.noreply.github.com>
2024-03-10 20:34:00 +0900

    2024.03.10

--- class1/[2920] scale.py
+++ class1/[2920] scale.py
@@ -1,7 +1,5 @@
 scale = list(map(int, input().split(" ")))
 
-print(type(scale))
-
 if(scale == sorted(scale)):
     print("ascending")
 elif( scale == sorted(scale, reverse = True)):
---
+++ class1/[3052] Remain.py
@@ -0,0 +1,7 @@
+a = []
+for i in range(10):
+    a.append(int(input())%42)
+
+a = set(a)
+print(len(a))
+    
